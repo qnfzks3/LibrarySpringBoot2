3daab7b458ba66d90108c6790204ecb9b2defa6c
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-05-22 17:54:28 +0900

    Library controller 인터페이스 , 클래스 구현

--- src/main/java/exam2/springboot/jpa/libraryspringboot2/controller/LibraryController.java
+++ src/main/java/exam2/springboot/jpa/libraryspringboot2/controller/LibraryController.java
@@ -1,16 +1,30 @@
 package exam2.springboot.jpa.libraryspringboot2.controller;
 
 
+import exam2.springboot.jpa.libraryspringboot2.service.LibraryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LibraryController {
 
-    @GetMapping("/")
-    public String list(){
+    @Autowired
+    LibraryService lbsrv;
 
-        return "list";
+
+    @GetMapping("/")
+    public ModelAndView list(Integer cpg){
+
+        ModelAndView mv = new ModelAndView();
+
+        if (cpg==null || cpg==0) cpg=1;
+
+        mv.addObject("lblist",lbsrv.readLibrary(cpg));
+    //html에 lblist로 만들었으니까 여기에 넘겨주기위해 이름을 써줌
+        mv.setViewName("list");
+        return mv;
 
     }
 
