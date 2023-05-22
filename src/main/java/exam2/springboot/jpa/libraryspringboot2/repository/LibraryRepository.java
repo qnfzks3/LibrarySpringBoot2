3d114bb7a78765731c1cd786f382eea6c4c436df
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-05-22 17:56:46 +0900

    Library Repository 인터페이스 구현

---
+++ src/main/java/exam2/springboot/jpa/libraryspringboot2/repository/LibraryRepository.java
@@ -0,0 +1,19 @@
+package exam2.springboot.jpa.libraryspringboot2.repository;
+
+import exam2.springboot.jpa.libraryspringboot2.model.Library;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.awt.print.Pageable;
+
+public interface LibraryRepository extends PagingAndSortingRepository<Library,Long> { //jpa에서 레포지터리 가져온다.
+
+    Page findAll(Pageable paging);    //page 으로 전체를 가져와라   , page 클래스
+
+
+    
+
+
+
+}
